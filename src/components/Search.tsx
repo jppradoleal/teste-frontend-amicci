d76835e14f11f2340569abed42354d5c12c464e4
João Pedro Prado <joaopedro0128@hotmail.com>
2024-04-25 17:18:06 -0300

    feat: adds interaction to screen

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,16 @@
 import { FormEventHandler } from "react"
+import { useStore } from "../store"
 
 interface SearchProps {
   onSubmit: FormEventHandler
 }
 
 export function Search ({ onSubmit }: Readonly<SearchProps>) {
+  const loadAddress = useStore(state => state.loadAddress)
+
   return <form onSubmit={onSubmit}>
     <input type="search" />
-    <button>Consultar</button>
-    <button>Minha localização</button>
+    <button type="submit">Consultar</button>
+    <button onClick={loadAddress} type="button">Minha localização</button>
   </form>
 }
--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -1,14 +1,21 @@
 import { FormEvent } from "react";
 import { Search } from "../../components/Search";
+import { useStore } from "../../store";
 
 export function Weather() {
-  function onSubmit(e: FormEvent) {
+  const { weather, loadWeather } = useStore(state => ({
+    weather: state.weather,
+    loadWeather: state.loadWeather
+  }))
+  
+  async function onSubmit(e: FormEvent) {
     e.preventDefault()
 
-    console.log("Submit weather")
+    await loadWeather()
   }
 
   return <main>
+    {JSON.stringify(weather)}
     <Search onSubmit={onSubmit} />
   </main>
 }
